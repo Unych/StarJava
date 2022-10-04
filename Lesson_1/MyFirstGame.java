65adae21c13d2b265a22568fcf3030671a7fb9b2
Dima <bad123457@gmail.com>
2022-10-04 17:50:39 +0300

    final lesson1

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,11 +1,27 @@
 public class MyFirstGame {
 
     public static void main(String[] args) {
+        int hiddenNum = 40;
+        int userNum = 20;
+        int i = 0;
+        while (hiddenNum != userNum) {
+            i++;
+            if (i == 2) {
+                userNum = 60;
+            } else if (i == 3) {
+                userNum = 30;
+            } else if (i == 4) {
+                userNum = 45;
+            } else if (i == 5)
+                userNum = 40;
 
-
-
-
-
-
+        if (hiddenNum > userNum) {
+            System.out.println("Число " + userNum + " меньше того, что загадал компьютер");
+        } else if (hiddenNum < userNum) {
+            System.out.println("Число " + userNum + " больше того, что загадал компьютер");
+        } else {
+            System.out.println("Вы победили! число " + userNum + " = " + hiddenNum);
+        }
+        }
     }
 }
