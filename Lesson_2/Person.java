a81c529b8e75972ffc43a9af399b35ce71df0e1a
Dima <bad123457@gmail.com>
2022-10-06 14:42:02 +0300

    Lesson_2 - pesron

---
+++ Lesson_2/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+
+    char gender = 'm';
+    String name = "Dima";
+    float weight = 82.5f;
+    int age = 26;
+    int height = 182;
+
+    void move() {
+        System.out.println("Двигается");
+    }
+
+    void sleep() {
+        System.out.println("Спит");
+    }
+
+    void speak() {
+        System.out.println("Говорит");
+    }
+
+    void learnJava() {
+        System.out.println("Учит Java");
+    }
+
+}
