e11b7c50741ad224472d01ea54255f7ecdb43037
Dima <bad123457@gmail.com>
2022-10-06 19:27:19 +0300

    Wolf - Lesson_2

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,28 +1,72 @@
 public class Wolf {
 
-    char gender = 'm';
-    String name = "Bars";
-    String color = "White";
-    double weight = 90.7;
-    int age = 5;
+    private char gender;
+    private String name;
+    private String color;
+    private double weight;
+    private int age;
 
-    void move() {
+    public char getGender() {
+        return gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    void setColor(String color) {
+        this.color = color;
+    }
+
+    void setWight(double weight) {
+        this.weight = weight;
+    }
+
+    void setAge(int age) {
+        if(age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public void move() {
         System.out.println("Идти");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("Сидеть");
     }
 
-    void run() {
+    public void run() {
         System.out.println("Бежит");
     }
 
-    void howl() {
+    public void howl() {
         System.out.println("Воет");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("Охотиться");
     }
 
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,7 +2,14 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        System.out.println("Имя волка " + wolfOne.name);
-        wolfOne.hunt();
+        wolfOne.setName("Барсик");
+        wolfOne.setGender('М');
+        wolfOne.setColor("Белый");
+        wolfOne.setAge(8);
+        wolfOne.setWight(96.6);
+        System.out.println("Имя волка " + wolfOne.getName() + "\nВозраст " + wolfOne.getAge()
+                + "\nПол " + wolfOne.getGender() + "\nЦвет волка " + wolfOne.getColor() + "\nВес " + 
+                wolfOne.getColor());
+
     }
 }
