54fbc7809bd6d266002bf50d9e8137808836b3fe
Dima <bad123457@gmail.com>
2022-10-06 15:22:24 +0300

    ООП

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,29 @@
+public class Wolf {
+
+    char gender = 'm';
+    String name = "Bars";
+    String color = "White";
+    double weight = 90.7;
+    int age = 5;
+
+    void move() {
+        System.out.println("Идти");
+    }
+
+    void sit() {
+        System.out.println("Сидеть");
+    }
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void howl() {
+        System.out.println("Воет");
+    }
+
+    void hunt() {
+        System.out.println("Охотиться");
+    }
+
+}
---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,8 @@
+public class WolfTest {
+
+    public static void main(String[] args) {
+        Wolf wolfOne = new Wolf();
+        System.out.println("Имя волка " + wolfOne.name);
+        wolfOne.hunt();
+    }
+}
