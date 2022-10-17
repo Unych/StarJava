9ce0868978f9d3d477b0196d4c4da589dba64949
Dima <bad123457@gmail.com>
2022-10-17 19:50:21 +0300

    package_1

---
+++ src/com/startjava/lesson_1/final/Сalculator.java
@@ -0,0 +1,28 @@
+package com.startjava.lesson_1.final;
+
+public class Сalculator {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        char sign = '^';
+        int result = 0;
+        if(sign == '+') {
+            result = num1 + num2;
+        } else if(sign == '-') {
+            result = num1 - num2;
+        } else if(sign == '*') {
+            result = num1 * num2;
+        } else if(sign == '/') {
+            result = num1 / num2;
+        } else if(sign == '%') {
+            result = num1 % num2;
+        } else if(sign == '^') {
+            result = 1;
+            for(int i = 1; i <= num2; i++) {
+                result *= num1;
+            }
+        }
+        System.out.print(num1 + " " + sign + " " + num2 + " = " + result);
+    }
+}
